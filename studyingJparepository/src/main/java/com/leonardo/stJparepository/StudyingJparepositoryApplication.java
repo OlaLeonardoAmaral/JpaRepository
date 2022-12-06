edae249497c36d5487419396fc918e56adb459f5
OlaLeonardoAmaral <leonardo3442@hotmail.com>
2022-12-06 20:01:18 -0300

    dependency

---
+++ studyingJparepository/src/main/java/com/leonardo/stJparepository/StudyingJparepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.stJparepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingJparepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingJparepositoryApplication.class, args);
+	}
+
+}
---
+++ studyingJparepository/src/test/java/com/leonardo/stJparepository/StudyingJparepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.stJparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingJparepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
