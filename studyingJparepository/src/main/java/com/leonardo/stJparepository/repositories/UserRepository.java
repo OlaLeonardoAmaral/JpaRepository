e4fa3ff69d947b30778257ffd62ebcc8d1dca648
OlaLeonardoAmaral <leonardo3442@hotmail.com>
2022-12-06 21:25:02 -0300

    search by name

--- studyingJparepository/src/main/java/com/leonardo/stJparepository/repositories/UserRepository.java
+++ studyingJparepository/src/main/java/com/leonardo/stJparepository/repositories/UserRepository.java
@@ -4,12 +4,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.leonardo.stJparepository.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary >= :maxSalary")
 	Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
+
+	@Query(value = "SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<User> searchName(String name, Pageable pageable);
+	
 	
 }
