9fa5a8a519acc0187f57f9001e805c08faa17597
OlaLeonardoAmaral <leonardo3442@hotmail.com>
2022-12-06 21:11:32 -0300

    custom search

--- studyingJparepository/src/main/java/com/leonardo/stJparepository/repositories/UserRepository.java
+++ studyingJparepository/src/main/java/com/leonardo/stJparepository/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.leonardo.stJparepository.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.leonardo.stJparepository.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary >= :maxSalary")
+	Page<User> searchSalary(Double minSalary, Double maxSalary, Pageable pageable);
 	
 }
